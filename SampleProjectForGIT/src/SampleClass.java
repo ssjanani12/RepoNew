3c63ee17cabe64c242341639af1b03b3c2b0f3c0
ssjanani12 <ssjanani12@gmail.com>
2022-04-12 22:11:14 +0530

    Add to master

---
+++ SampleProjectForGIT/src/SampleClass.java
@@ -0,0 +1,21 @@
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		addition();
+		}
+		
+		
+		public static void addition()
+		{
+			int a= 10;
+			int b=20;
+			int c= a+b;		
+			System.out.println(c);
+
+		}
+
+
+	}
+
+}
